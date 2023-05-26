8f376cbfeaa98e2f82dace823b3833b1f0e4d95b
Abhishek <abhij05082002@gmail.com>
2023-05-26 11:24:38 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express=require('express');
 const app=express();
-const port=8003;
+const port=process.env.PORT ||8003;
 const mongoose=require('mongoose')
 require('./db/Conn')
 const users=require('./models/userSchema')
